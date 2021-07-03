8f885bf1bd6dae8a3ddd3c602d2913aefae110ae
slayerofdrosophila <jliu8150@gmail.com>
2021-07-03 18:41:33 -0400

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,14 +4,14 @@ module.exports = {
     'googleAuth' : {
         'clientID': process.env.Google_OAuth_App_ClientID,
         'clientSecret'  : process.env.Google_OAuth_App_ClientSecret,
-        'callbackURL'   : 'http://hangman-royale.herokuapp.com:' + process.env.PORT + '/login/authorized'
+        'callbackURL'   : 'https://hangman-royale.herokuapp.com/login/authorized'
         // 'callbackURL'   : 'http://127.0.0.1:5000/login/authorized'
     },
 
     'googleAuth2' : {
         'clientID': process.env.Google_OAuth_App_ClientID,
         'clientSecret'  : process.env.Google_OAuth_App_ClientSecret,
-        'callbackURL'   : 'http://hangman-royale.herokuapp.com:' + process.env.PORT + '/login/authorized'
+        'callbackURL'   : 'https://hangman-royale.herokuapp.com/login/authorized'
         // 'callbackURL'   : 'http://127.0.0.1:5000/login/authorized'
     },
 
